c0be2e4f2277144cf2c207337d4c807f6c941902
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-01-04 10:34:47 +0600

    removing .git directory after clonning

---
+++ testing/src/routes/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import { AuthRoutes } from '../app/modules/auth/auth.route';
+import { UserRoutes } from '../app/modules/user/user.route';
+import { RandomRoutes } from '../app/modules/random/random.route';
+const router = express.Router();
+
+const apiRoutes = [
+{
+    path: '/user',
+    route: UserRoutes,
+  },
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+  {
+    path: '/random',
+    route: RandomRoutes,
+  }
+];
+
+apiRoutes.forEach(route => router.use(route.path, route.route));
+
+export default router;
